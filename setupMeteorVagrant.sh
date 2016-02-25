584e28b17b08414986495e574b5d614db6c187c2
Pete Turnbull <pete.j.turnbull@gmail.com>
2016-02-25 23:49:44 +0000

    Changed script to source bashrc at the same time

--- setupMeteorVagrant.sh
+++ setupMeteorVagrant.sh
@@ -1,1 +1,1 @@
-sudo mount --bind /home/vagrant/meteor-blog/.meteor/local/ /vagrant/meteor-blog/.meteor/local/
+echo "sudo mount --bind /home/vagrant/meteor-blog/.meteor/local/ /vagrant/meteor-blog/.meteor/local/" >> ~/.bashrc && source ~/.bashrc
